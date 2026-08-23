9a4132f7b23788fd523aad78116b2854a6c5dd30
takgeun-O <takgeun92@gmail.com>
2026-08-24 00:13:11 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,38 @@
 "use strict";
+
+// 카테고리 버튼 클릭 시 클릭한 카테고리만 가지고 있는 타입에 따라서
+// 프로젝트마다의 데이터에 맞는 것만 나오도록 (filter 사용)
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+
+categories.addEventListener("click", (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+
+  handleActiveSelection(event.target);
+
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projectsContainer.classList.add("anim-out");
+  projects.forEach((project) => {
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+}

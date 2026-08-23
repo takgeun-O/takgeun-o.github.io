272b84b27c5b00fc64e9adedec54fe00758d0969
takgeun-O <takgeun92@gmail.com>
2026-08-23 22:36:56 +0900

    JS-Arrowup

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
 // Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
 
-document.addEventListener('scroll', () => {
-    
-    if(window.scrollY > headerHeight) {
-        header.classList.add('header--dark');
-    } else {
-        header.classList.remove('header--dark');
-    }
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add("header--dark");
+  } else {
+    header.classList.remove("header--dark");
+  }
 });
 
 // 아래로 스크롤할수록 Home 투명화
-const home = document.querySelector('.home__container');
+const home = document.querySelector(".home__container");
 const homeHeight = home.offsetHeight;
-document.addEventListener('scroll', () => {
-    const calculatedOpacity = (homeHeight - window.scrollY) / homeHeight;
-    const opacity = Math.max(0, Math.min(1, calculatedOpacity));
-    home.style.opacity = opacity;
-})
+document.addEventListener("scroll", () => {
+  const calculatedOpacity = (homeHeight - window.scrollY) / homeHeight;
+  const opacity = Math.max(0, Math.min(1, calculatedOpacity));
+  home.style.opacity = opacity;
+});
+
+// Home 섹션 절반 정도 이상이 가려지면 Arrow button 노출시키기
+const arrow = document.querySelector(".arrow-up");
+document.addEventListener(
+  "scroll",
+  () => {
+    if (window.scrollY >= homeHeight / 2) {
+      arrow.style.opacity = 1;
+    } else {
+      arrow.style.opacity = 0;
+    }
+  },
+  { passive: true },
+);
